90a9c8d9772c3ea5e014bdca6f5b45aaffd59d82
Ojo Triumph <ojotriumph4@gmail.com>
2023-09-20 02:55:16 +0100

    fixed events design

--- pages/edit/event/[id].js
+++ pages/edit/event/[id].js
@@ -410,7 +410,7 @@ const event = () => {
 				</form>
 			</main>
 
-			{openEventPreview ? <div className='fixed top-[50%] left-[50%] w-[90%] md:w-[60%] mx-auto bg-white shadow-[#FFD95A] z-[50] px-10 py-7 h-[80vh] overflow-auto' style={{ transform: 'translate(-50%, -50%)' }}>
+			{openEventPreview ? <div className='fixed top-[50%] left-[50%] w-[90%] md:w-[60%] mx-auto bg-white shadow-[#FFD95A] z-[50] px-10 py-7 h-[95vh] md:h-[80vh] overflow-auto' style={{ transform: 'translate(-50%, -50%)' }}>
 				<Image src={flier} width={300} height={300} className="h-[30vh] md:h-[45vh] flex flex-row md:flex-col items-center justify-center bg-[#FFD95A] registergray w-full bg-cover mt-7 rounded-xl" />
 				<div className="px-1 md:px-4 py-2 md:py-6 flex flex-col lg:flex-row items-start mx-auto lg:mt-3 gap-x-12">
 					<div className='px-4 flex flex-col gap-y-4 lg:gap-y-7'>
